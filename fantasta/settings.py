293ab842f544e198c58c304282bffb818bcd88e3
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-02 22:55:12 +0200

    force dev. env.

--- fantasta/settings.py
+++ fantasta/settings.py
@@ -2,12 +2,6 @@ from os import environ, getenv
 from pathlib import Path
 
 
-DOMAIN = 'fantasta.net'
-ENV = getenv('ENVIRONMENT', 'dev')
-
-if ENV == 'prod':
-    ADMINS = [('Ruggero Fabbiano', 'ruggero_fabbiano@outlook.com')]
-    ALLOWED_HOSTS = [DOMAIN, 'fantasta.eu-west-3.elasticbeanstalk.com']
 APPEND_SLASH = False
 ASGI_APPLICATION = 'fantasta.asgi.application'
 AUTH_PASSWORD_VALIDATORS = [
@@ -16,32 +10,20 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
 ]
-if ENV == 'prod':
-    AWS_ACCESS_KEY_ID = environ['AWS_ID']
-    AWS_SECRET_ACCESS_KEY = environ['AWS_KEY'] 
 BASE_DIR = Path(__file__).resolve().parent.parent
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1" if ENV == 'dev' else DOMAIN, 6379)]
-        }
-    }
-}
 CRISPY_ALLOWED_TEMPLATE_PACKS = CRISPY_TEMPLATE_PACK = 'bootstrap5'
-if ENV == 'prod':
-    CSRF_COOKIE_SECURE = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
-DEBUG = ENV == 'dev'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'ruggero_fabbiano@outlook.com'
 DJANGO_EASY_HEALTH_CHECK = {'PATH': "/health-check"}
+DOMAIN = 'fantasta.net'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ENV = getenv('ENVIRONMENT', 'dev')
 INSTALLED_APPS = [
     # 'daphne',
     'django.contrib.admin',
@@ -82,26 +64,6 @@ LOGGING = {
         'fantasta': {'handlers': ['console'], 'level': 'DEBUG'}
     }
 }
-if ENV == 'prod':
-    LOGGING['formatters'] = {
-        'custom': {
-            'style': '{',
-            'format': '{levelname} {asctime} {message}'
-        }
-    }
-    LOGGING['handlers']['fantasta'] = {
-        'class': 'logging.FileHandler',
-        'filename': F'/var/log/fantasta.log',
-        'level': 'DEBUG',
-        'formatter': 'custom'
-    }
-    LOGGING['loggers']['fantasta']['handlers'].append('fantasta')
-    LOGGING['loggers']['django'] = {
-        'handlers': ['fantasta'], 'level': 'INFO', 'propagate': False
-    }
-    LOGGING['loggers']['django.request'] = {
-        'handlers': ['mail_admins'], 'level': 'ERROR', 'propagate': False
-    } 
 LOGIN_REDIRECT_URL = 'waiting_room'
 LOGIN_URL = 'log-in'
 LOGOUT_REDIRECT_URL = '/'
@@ -120,15 +82,6 @@ SECRET_KEY = getenv(
     'DJANGO_KEY',
     'django-insecure-6e3bzkjtu(9*#20y@du$(*e-kj57jh)jzoabzk!dn793wn9x=#'
 )
-if ENV == 'prod':
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_HSTS_SECONDS = 31536000 # 1 year
-    SECURE_REDIRECT_EXEMPT = ['/health-check']
-    SECURE_SSL_REDIRECT = True
-    SERVER_EMAIL = 'ruggero_fabbiano@outlook.com'
-    SESSION_COOKIE_SECURE = True
-    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 STATICFILES_DIRS = [BASE_DIR / 'fantasta' / 'static']
 STATIC_ROOT = 'static'
 STATIC_URL = 'static/'
@@ -149,3 +102,53 @@ TEMPLATES = [
 ]
 TIME_ZONE = 'Europe/Rome'
 WSGI_APPLICATION = 'fantasta.wsgi.application'
+
+
+ENV = 'dev'
+ALLOWED_HOSTS = [DOMAIN, 'fantasta.eu-west-3.elasticbeanstalk.com']
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1" if ENV == 'dev' else DOMAIN, 6379)]
+        }
+    }
+}
+DEBUG = ENV == 'dev'
+
+
+if ENV == 'prod':
+    ADMINS = [('Ruggero Fabbiano', 'ruggero_fabbiano@outlook.com')]
+    ALLOWED_HOSTS = [DOMAIN, 'fantasta.eu-west-3.elasticbeanstalk.com']
+    AWS_ACCESS_KEY_ID = environ['AWS_ID']
+    AWS_SECRET_ACCESS_KEY = environ['AWS_KEY'] 
+    CSRF_COOKIE_SECURE = True
+    LOGGING['formatters'] = {
+        'custom': {
+            'style': '{',
+            'format': '{levelname} {asctime} {message}'
+        }
+    }
+    LOGGING['handlers']['fantasta'] = {
+        'class': 'logging.FileHandler',
+        'filename': F'/var/log/fantasta.log',
+        'level': 'DEBUG',
+        'formatter': 'custom'
+    }
+    LOGGING['loggers']['fantasta']['handlers'].append('fantasta')
+    LOGGING['loggers']['django'] = {
+        'handlers': ['fantasta'], 'level': 'INFO', 'propagate': False
+    }
+    LOGGING['loggers']['django.request'] = {
+        'handlers': ['mail_admins'], 'level': 'ERROR', 'propagate': False
+    }
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_HSTS_PRELOAD = True
+    # SECURE_HSTS_SECONDS = 31536000 # 1 year
+    # SECURE_REDIRECT_EXEMPT = ['/health-check']
+    # SECURE_SSL_REDIRECT = True
+    # SERVER_EMAIL = 'ruggero_fabbiano@outlook.com'
+    # SESSION_COOKIE_SECURE = True
+    # SESSION_EXPIRE_AT_BROWSER_CLOSE = True
--- fantasta/wsgi.py
+++ fantasta/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for fantasta project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
+from os import environ
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
 
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
 application = get_wsgi_application()
