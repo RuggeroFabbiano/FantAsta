622d03de224369b5e92b21ae2c6d4821533701d9
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-02 15:20:21 +0200

    misc.

--- fantasta/settings.py
+++ fantasta/settings.py
@@ -113,6 +113,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 ROOT_URLCONF = 'fantasta.urls'
