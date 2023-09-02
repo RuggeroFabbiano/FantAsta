aaa58679328dd50c0271de02e7f00647f5968be4
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-02 02:24:10 +0200

    prepare to first deployment

--- fantasta/settings.py
+++ fantasta/settings.py
@@ -1,13 +1,46 @@
+from os import environ, getenv
 from pathlib import Path
 
 
+ENV = getenv('ENVIRONMENT', 'dev')
+
+if ENV == 'prod':
+    ADMINS = [('Ruggero Fabbiano', 'ruggero.fabbiano@gmail.com')]
+    ALLOWED_HOSTS = ['test.leano.fr', 'test.eu-west-3.elasticbeanstalk.com']
+APPEND_SLASH = False
+ASGI_APPLICATION = 'fantasta.asgi.application'
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+]
+if ENV == 'prod':
+    AWS_ACCESS_KEY_ID = environ['AWS_ID']
+    AWS_SECRET_ACCESS_KEY = environ['AWS_KEY'] 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = 'django-insecure-6e3bzkjtu(9*#20y@du$(*e-kj57jh)jzoabzk!dn793wn9x=#'
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)]
+        }
+    }
+}
+CRISPY_ALLOWED_TEMPLATE_PACKS = CRISPY_TEMPLATE_PACK = 'bootstrap5'
+if ENV == 'prod':
+    CSRF_COOKIE_SECURE = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
+}
+DEBUG = ENV == 'dev'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = 'ruggero.fabbiano@gmail.com'
+DJANGO_EASY_HEALTH_CHECK = {'PATH': "/health-check"}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 INSTALLED_APPS = [
     'daphne',
     'django.contrib.admin',
@@ -22,19 +55,83 @@ INSTALLED_APPS = [
     'account',
     'auction'
 ]
-
+LANGUAGE_CODE = 'it-it'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {'()': 'django.utils.log.RequireDebugFalse'},
+        'require_debug_true': {'()': 'django.utils.log.RequireDebugTrue'}
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'filters': ['require_debug_true']
+        },
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+            'filters': ['require_debug_false']
+        },
+        'null': {'class': 'logging.NullHandler'}
+    },
+    'loggers': {
+        'django.security.DisallowedHost': {
+            'handlers': ['null'], 'propagate': False
+        },
+        'fantasta': {'handlers': ['console'], 'level': 'DEBUG'}
+    }
+}
+if ENV == 'prod':
+    LOGGING['formatters'] = {
+        'custom': {
+            'style': '{',
+            'format': '{levelname} {asctime} {message}'
+        }
+    }
+    LOGGING['handlers']['fantasta'] = {
+        'class': 'logging.FileHandler',
+        'filename': F'/var/log/fantasta.log',
+        'level': 'DEBUG',
+        'formatter': 'custom'
+    }
+    LOGGING['loggers']['fantasta']['handlers'].append('fantasta')
+    LOGGING['loggers']['django'] = {
+        'handlers': ['fantasta'], 'level': 'INFO', 'propagate': False
+    }
+    LOGGING['loggers']['django.request'] = {
+        'handlers': ['mail_admins'], 'level': 'ERROR', 'propagate': False
+    } 
+LOGIN_REDIRECT_URL = 'waiting_room'
+LOGIN_URL = 'log-in'
+LOGOUT_REDIRECT_URL = '/'
 MIDDLEWARE = [
+    'easy_health_check.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
-
 ROOT_URLCONF = 'fantasta.urls'
-
+SECRET_KEY = getenv(
+    'DJANGO_KEY',
+    'django-insecure-6e3bzkjtu(9*#20y@du$(*e-kj57jh)jzoabzk!dn793wn9x=#'
+)
+if ENV == 'prod':
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_SECONDS = 31536000 # 1 year
+    SECURE_REDIRECT_EXEMPT = ['/health-check']
+    SECURE_SSL_REDIRECT = True
+    SERVER_EMAIL = 'ruggero.fabbiano@gmail.com'
+    SESSION_COOKIE_SECURE = True
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+STATICFILES_DIRS = [BASE_DIR / 'fantasta' / 'static']
+STATIC_ROOT = 'static'
+STATIC_URL = 'static/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -50,53 +147,5 @@ TEMPLATES = [
         }
     }
 ]
-
-ASGI_APPLICATION = 'fantasta.asgi.application'
-WSGI_APPLICATION = 'fantasta.wsgi.application'
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)]
-        }
-    }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
-}
-
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
-]
-
-
-LANGUAGE_CODE = 'it-it'
 TIME_ZONE = 'Europe/Rome'
-
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'fantasta' / 'static']
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'ruggero.fabbiano@gmail.com'
-
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = CRISPY_TEMPLATE_PACK = 'bootstrap5'
-
-
-LOGIN_URL = 'log-in'
-LOGIN_REDIRECT_URL = 'waiting_room'
-LOGOUT_REDIRECT_URL = '/'
+WSGI_APPLICATION = 'fantasta.wsgi.application'
