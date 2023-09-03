1356b847d3c91de7aa607b68070cd0216cf65ca8
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-03 14:09:32 +0200

    tweak admin.

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
 
 
+class Logs(TemplateView):
+    """Show list of server logs"""
+
+    template_name = 'admin/logs.html'
+
+
 class Log(TemplateView):
     """Show log content"""
 
@@ -9,6 +15,7 @@ class Log(TemplateView):
     def get_context_data(self, **kwargs):
         """Pass log content to template"""
         context = super().get_context_data(**kwargs)
-        with open('/var/log/fantasta.log', 'r', encoding='UTF8') as log:
+        file = kwargs['log']
+        with open(F'/var/log/{file}.log', 'r', encoding='UTF8') as log:
             context['content'] = log.read().strip('\n')
         return context
--- fantasta/urls.py
+++ fantasta/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, re_path
 
-from admin.views import Log
+from admin import views as logs
 from auction import views as auction
 from .consumers import Consumer
 from . import views
 
 
 urlpatterns = [
-    path("admin/log", Log.as_view(), name="admin-log"),
+    path("admin/logs/<str:log>", logs.Logs.as_view(), name="admin-logs"),
+    path("admin/log", logs.Log.as_view(), name="admin-log"),
     path('admin/', admin.site.urls),
     path('', views.Home.as_view()),
     path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
