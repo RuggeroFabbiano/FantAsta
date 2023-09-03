a260cfa6e9eed07d6290d80e56998c4cca4d3e7a
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-03 12:48:12 +0200

    populate Auction

--- auction/admin.py
+++ auction/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.admin import ModelAdmin, site
+from import_export.admin import ImportExportModelAdmin
+from import_export.resources import ModelResource
 
-# Register your models here.
+from .models import Club, Player
+
+
+site.register(Club, ModelAdmin)
+
+
+@register(Player)
+class PlayerAdmin(ImportExportModelAdmin):
+
+    class PlayerResource(ModelResource):
+        class Meta:
+            model = Player
+
+    resource_class = PlayerResource
--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import (CASCADE, SET_NULL, CharField, ForeignKey, Model,
+                              OneToOneField, PositiveIntegerField)
 
-# Create your models here.
+
+ROLES = [
+    ('P', 'portiere'),
+    ('D', 'difensore'),
+    ('C', 'centrocampista'),
+    ('A', 'attaccante')
+]
+
+
+class Club(Model):
+    """Teams participating to the ligue"""
+
+    name = CharField(max_length=50)
+    money = PositiveIntegerField(default=500)
+    next_call = CharField(max_length=1, choices=ROLES, null=True)
+    user = OneToOneField(User, CASCADE)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def label(self):
+        """
+        Associate to the team an anonymised label indexed by
+        alphabetical order
+        """
+        return F"team{Club.objects.filter(name__lte=self.name).count()}"
+
+
+class Player(Model):
+    """Serie A football players"""
+
+    name = CharField(max_length=50)
+    team = CharField(max_length=50)
+    role = CharField(max_length=1, choices=ROLES)
+    price = PositiveIntegerField()
+    club = ForeignKey(Club, SET_NULL, "players", null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return F"{self.name} ({self.team})"
