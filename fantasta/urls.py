47b1ac74546a01ae50591925e6d7d2ae1d2212ba
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-03 12:50:19 +0200

    add missing path

--- fantasta/urls.py
+++ fantasta/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home.as_view()),
     # path("conto/", include("account.urls")),
-    # path('regole', views.Rules.as_view(), name='rules'),
+    path('regole', views.Rules.as_view(), name='rules'),
     # path('asta', viewsAuction.Room.as_view(), name='auction'),
     # path('players/<str:role>', viewsAuction.PlayerList.as_view(), name='players')
 ]
