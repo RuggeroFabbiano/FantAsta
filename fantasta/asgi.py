23e6b71e6ebf2306dd6a0d0270d140b584107afe
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-04 09:44:59 +0200

    a step from Chat to FantAsta

--- fantasta/asgi.py
+++ fantasta/asgi.py
@@ -1,45 +1,22 @@
-# from os import environ
-
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from django.core.asgi import get_asgi_application
-
-# # import .urls
-# from .urls import socket_patterns
-
-
-# environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
-# application = ProtocolTypeRouter(
-#     {
-#         'http': get_asgi_application(),
-#         'websocket':
-#             AllowedHostsOriginValidator(
-#                 AuthMiddlewareStack(URLRouter(socket_patterns))
-#             )
-#     }
-# )
-
-
-import os
+from os import environ
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+# import .urls
+# from .urls import socket_patterns
 
+
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
 import auction.routing
-
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(auction.routing.websocket_urlpatterns))
-        ),
+        'http': get_asgi_application(),
+        'websocket':
+            AllowedHostsOriginValidator(
+                AuthMiddlewareStack(URLRouter(auction.routing.websocket_urlpatterns))  # socket_patterns
+            )
     }
 )
