b09d27b6fb07def1f2f6ca1653493a5868eed7b4
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-04 10:48:26 +0200

    a step to FantAsta 9

--- fantasta/urls.py
+++ fantasta/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 # from admin import views as logs
 # from auction import views as auction
 # from .consumers import Consumer
-# from . import views
+from . import views
 
 
 # urlpatterns = [
@@ -24,7 +24,7 @@ urlpatterns = [
     # path("admin/logs/<str:log>", logs.Log.as_view(), name="admin-log"),
     path("admin/", admin.site.urls),
     path("chat/", include("auction.urls")),  #! TEMP.
-    # path('', views.Home.as_view()),
+    path('', views.Home.as_view()),
     # path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
     # path('connessione', views.SignIn.as_view(), name="log-in"),
     # path('regole', views.Rules.as_view(), name='rules'),
