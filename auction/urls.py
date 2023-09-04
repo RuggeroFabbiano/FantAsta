36c23266438f1df302decc0ad796253996598a8d
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-04 09:56:48 +0200

    a step to FantAsta 5

--- auction/urls.py
+++ auction/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 
+from .consumers import ChatConsumer
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
+]
+
+socket_patterns = [
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
 ]
--- fantasta/asgi.py
+++ fantasta/asgi.py
@@ -5,10 +5,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from auction.routing import socket_patterns
-
-# import .urls
-# from .urls import socket_patterns
+# from auction.routing import socket_patterns
+from auction.urls import socket_patterns
 
 
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
