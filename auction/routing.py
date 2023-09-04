c228b3594f2e67c05d3e71259fcb1aae7359835c
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-04 09:50:22 +0200

    a step to FantAsta 3

--- auction/routing.py
+++ auction/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 
 from . import consumers
 
-websocket_urlpatterns = [
+socket_patterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
--- fantasta/asgi.py
+++ fantasta/asgi.py
@@ -5,18 +5,19 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from auction.routing import websocket_urlpatterns
+
 # import .urls
 # from .urls import socket_patterns
 
 
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
-# import auction.routing
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
         'websocket':
             AllowedHostsOriginValidator(
-                AuthMiddlewareStack(URLRouter(auction.routing.websocket_urlpatterns))  # socket_patterns
+                AuthMiddlewareStack(URLRouter(socket_patterns))
             )
     }
 )
