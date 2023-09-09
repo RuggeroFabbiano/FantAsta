a3014f57164ec9a14b5811360655827648c8157a
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-09 15:26:12 +0200

    Temp. comment Auth view

--- fantasta/urls.py
+++ fantasta/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("asta/", include("auction.urls")),  #! TEMP.
     # path('', views.Home.as_view()),
     path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
-    path('connessione', views.SignIn.as_view(), name="log-in"),
+    # path('connessione', views.SignIn.as_view(), name="log-in"),
 ]
