0194661dbf77836046d91cc030edbc6f62675769
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-09 21:01:02 +0200

    Auction views

--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+
+from auction.models import Club, Player
 
 
 class Rules(LoginRequiredMixin, TemplateView):
@@ -9,7 +12,40 @@ class Rules(LoginRequiredMixin, TemplateView):
     template_name = 'auction/rules.html'
 
 
+class Room(LoginRequiredMixin, TemplateView):
+    """Auction room"""
 
+    template_name = 'auction/auction.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Provide participant list both as objects and as serialised data
+        """
+        context = super().get_context_data(**kwargs)
+        clubs = Club.objects.all()
+        context['participants'] = clubs
+        context['clubs'] = list(clubs.values_list('name', flat=True))
+        return context
+
+
+class PlayerList(View):
+    """On AJAX requests, send list of player for current round"""
+
+    def get(self, request, *args, **kwargs):
+        """GET request"""
+        players = Player.objects.filter(role=kwargs['role'], club__isnull=True)
+        data = [{'id': p.id, 'name': p.name, 'team': p.team, 'price': p.price} for p in players]
+        return JsonResponse(data, safe=False)
+
+
+# class PlayerDetail(View):
+#     """On AJAX requests, send details of chosen player"""
+
+#     def get(self, request, *args, **kwargs):
+#         """GET request"""
+#         players = Player.objects.filter(role=kwargs['role'], club__isnull=True)
+#         data = [{'id': p.id, 'name': p.name, 'team': p.team, 'price': p.price} for p in players]
+#         return JsonResponse(data, safe=False)
 
 
 
--- fantasta/urls.py
+++ fantasta/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("admin/log", Log.as_view(), name="admin-log"),
     path("admin/", admin.site.urls),
     path('', views.Home.as_view()),
-    path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
-    path('connessione', views.SignIn.as_view(), name="log-in"),
-    path("asta/", include("auction.urls"))
+    path("connessione/<int:id>", views.SendPassowrd.as_view(), name="sign-in"),
+    path("connessione", views.SignIn.as_view(), name="log-in"),
+    path("asta/", include('auction.urls'))
 ]
