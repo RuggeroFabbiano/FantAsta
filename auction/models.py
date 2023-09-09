95879b9f3d3cea0d6b39b34b0da6a2411403cd10
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-09 20:31:13 +0200

    add models

--- auction/models.py
+++ auction/models.py
@@ -1,50 +1,50 @@
-# from django.contrib.auth.models import User
-# from django.db.models import (CASCADE, SET_NULL, CharField, ForeignKey, Model,
-#                               OneToOneField, PositiveIntegerField)
+from django.contrib.auth.models import User
+from django.db.models import (CASCADE, SET_NULL, CharField, ForeignKey, Model,
+                              OneToOneField, PositiveIntegerField)
 
 
-# ROLES = [
-#     ('P', 'portiere'),
-#     ('D', 'difensore'),
-#     ('C', 'centrocampista'),
-#     ('A', 'attaccante')
-# ]
+ROLES = [
+    ('P', 'portiere'),
+    ('D', 'difensore'),
+    ('C', 'centrocampista'),
+    ('A', 'attaccante')
+]
 
 
-# class Club(Model):
-#     """Teams participating to the ligue"""
+class Club(Model):
+    """Teams participating to the ligue"""
 
-#     name = CharField(max_length=50)
-#     money = PositiveIntegerField(default=500)
-#     next_call = CharField(max_length=1, choices=ROLES, null=True)
-#     user = OneToOneField(User, CASCADE)
+    name = CharField(max_length=50)
+    money = PositiveIntegerField(default=500)
+    next_call = CharField(max_length=1, choices=ROLES, null=True)
+    user = OneToOneField(User, CASCADE)
 
-#     class Meta:
-#         ordering = ['name']
+    class Meta:
+        ordering = ['name']
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     @property
-#     def label(self):
-#         """
-#         Associate to the team an anonymised label indexed by
-#         alphabetical order
-#         """
-#         return F"team{Club.objects.filter(name__lte=self.name).count()}"
+    @property
+    def label(self):
+        """
+        Associate to the team an anonymised label indexed by
+        alphabetical order
+        """
+        return F"team{Club.objects.filter(name__lte=self.name).count()}"
 
 
-# class Player(Model):
-#     """Serie A football players"""
+class Player(Model):
+    """Serie A football players"""
 
-#     name = CharField(max_length=50)
-#     team = CharField(max_length=50)
-#     role = CharField(max_length=1, choices=ROLES)
-#     price = PositiveIntegerField()
-#     club = ForeignKey(Club, SET_NULL, "players", null=True)
+    name = CharField(max_length=50)
+    team = CharField(max_length=50)
+    role = CharField(max_length=1, choices=ROLES)
+    price = PositiveIntegerField()
+    club = ForeignKey(Club, SET_NULL, "players", null=True)
 
-#     class Meta:
-#         ordering = ['name']
+    class Meta:
+        ordering = ['name']
 
-#     def __str__(self):
-#         return F"{self.name} ({self.team})"
+    def __str__(self):
+        return F"{self.name} ({self.team})"
--- fantasta/urls.py
+++ fantasta/urls.py
@@ -4,14 +4,14 @@ from django.urls import include, path
 from admin.views import Log
 # from auction import views as auction
 # from .consumers import Consumer
-# from . import views
+from . import views
 
 
 urlpatterns = [
     path("admin/log", Log.as_view(), name="admin-log"),
     path("admin/", admin.site.urls),
-    # path("asta/", include("auction.urls")),
-    # path('', views.Home.as_view()),
-    # path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
-    # path('connessione', views.SignIn.as_view(), name="log-in"),
+    path('', views.Home.as_view()),
+    path('connessione/<int:id>', views.SendPassowrd.as_view(), name="sign-in"),
+    path('connessione', views.SignIn.as_view(), name="log-in"),
+    # path("asta/", include("auction.urls"))
 ]
