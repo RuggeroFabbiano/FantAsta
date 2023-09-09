da257a3f085b309ef2cf5aca1b62405fe7ef3a71
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-09 15:21:48 +0200

    Auth migration

--- deployment/start_app.sh
+++ deployment/start_app.sh
@@ -6,6 +6,7 @@ echo "\n----- Collect static files -----"
 python manage.py collectstatic --noinput
 
 echo "\n----- Apply migrations -----"
+python manage.py migrate auth
 python manage.py makemigrations 
 python manage.py migrate
 
