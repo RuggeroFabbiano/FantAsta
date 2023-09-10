5f413af5d2d1f036bdbc0770bf484d448677466d
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-10 18:09:45 +0200

    ASGI 2

--- fantasta/asgi.py
+++ fantasta/asgi.py
@@ -5,13 +5,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from auction.urls import socket_patterns
-
 
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
 asgi_app = get_asgi_application()
 
-import auction.urls
+from auction.urls import socket_patterns
 
 application = ProtocolTypeRouter(
     {
