a006bf9ced8abb21e57edeeba047f055f548cc82
RuggeroFabbiano <ruggero_fabbiano@outlook.com>
2023-09-10 15:38:36 +0200

    correct ASGI init.

--- fantasta/asgi.py
+++ fantasta/asgi.py
@@ -3,21 +3,21 @@ from os import environ
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django import setup
 from django.core.asgi import get_asgi_application
-import django
 
 from auction.urls import socket_patterns
 
 
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasta.settings')
-django.setup()
+asgi_app = get_asgi_application()
+
+# import auction.urls
+
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application(),
-        'websocket':
-            AllowedHostsOriginValidator(
-                AuthMiddlewareStack(URLRouter(socket_patterns))
-            )
+        'http': asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(socket_patterns))
+        )
     }
 )
